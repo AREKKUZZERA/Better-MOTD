0a9dbf7753b1aeef14de9fd3c51f2f22a257bb8c
AREKKUZZERA <arekkuzzera@gmail.com>
2026-01-14 15:06:40 +0300

    Fix CI: add missing java.util.Map import

--- src/main/java/bettermotd/ConfigModel.java
+++ src/main/java/bettermotd/ConfigModel.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.logging.Logger;
+import java.util.Map;
 
 public record ConfigModel(
         String selectionMode,
